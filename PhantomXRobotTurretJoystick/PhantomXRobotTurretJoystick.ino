c0f40e942f84c81aa7a869c5d4bbccbb5f3c012f
Kyle Granat <kyle@trossenrobotics.com>
2014-03-20 14:44:09 -0500

    Naming of #defines changed and general cleanup

--- PhantomXRobotTurretJoystick/PhantomXRobotTurretJoystick.ino
+++ PhantomXRobotTurretJoystick/PhantomXRobotTurretJoystick.ino
@@ -4,6 +4,7 @@
    
    -DEADBANDHIGH and DEADBANDLOW can be adjusted to the deadband of your particular joystick if needed
    -*_LIMIT defines are hard limits for pan and tilt to avoid the turret from damagint/over torque-ing itself
+   -int speed can be varied to change the overall speed of the system
    
   ArbotiX Firmware - Commander Extended Instruction Set Example
   Copyright (c) 2008-2010 Vanadium Labs LLC.  All right reserved.
@@ -38,12 +39,12 @@
 
 // the F2 'C' bracket attached to the tilt servo creates a physical limitation to how far
 // we can move the tilt servo. This software limit will ensure that we don't jam the bracket into the servo.
-#define TILT_UPPER_LIMIT 768 
-#define TILT_LOWER_LIMIT 256
+#define TILT_MAX 768 
+#define TILT_MIN 256
 
 //Upper/Lower limits for the pan servo - by defualt they are the normal 0-1023 (0-300) positions for the servo
-#define PAN_UPPER_LIMIT 1023 
-#define PAN_LOWER_LIMIT 0
+#define PAN_MAX 1023 
+#define PAN_MIN 0
 
 //Default/Home position. These positions are used for both the startup position as well as the 
 //position the servos will go to when they lose contact with the commander
@@ -69,30 +70,35 @@ BioloidController bioloid = BioloidController(1000000);  //create a bioloid obje
 
 int pan;    //current position of the pan servo
 int tilt;   //current position of the tilt servo  
-int panMod =1;  //modifier for pan speed on left joystick - increase this to reduce the turret's speed
-int tiltMod =1;  //modifier for tilt speed on left joystick - increase this to reduce the turret's speed
-int joyPanVal = 0;//current value of the pan joystick (analog 0)
-int joyTiltVal = 0;//current value of the tilt joystick (analog 1)
-int joyTiltMapped =0;//tilt joystick value, mapped from 1-1023 to -500-500
-int joyPanMapped =0;//pan joystick value, mapped from 1-1023 to -500-500
+
+int joyPanVal = 0;//current value of the pan joystick (analog 0) 0-1023
+int joyTiltVal = 0;//current value of the tilt joystick (analog 1) 0-1023
+int joyTiltMapped =0;//tilt joystick value, mapped  to -speed - speed
+int joyPanMapped =0;//pan joystick value, mapped to -speed - speed
+
 int speed = 50;//increase this to increase the speed of the movement
 
 
-void setup(){
+void setup()
+{
   
-  // setup LED
-  pinMode(0,OUTPUT);
+ 
+  pinMode(0,OUTPUT);     // setup user LED
+  digitalWrite(0, HIGH); // turn user LED on to show the program is running
+   
   
-  // setup interpolation, slowly raise turret to a 'home' positon. 1023 are the 'center' positions for both servos
+  // setup interpolation, slowly raise turret to a 'home' positon. 512 are the 'center' positions for both servos
   pan = DEFAULT_PAN;//load default pan value for startup
   tilt = DEFAULT_TILT;//load default tilt value for startup
-  delay(1000);
-  bioloid.poseSize = 2;//2 servos, so the pose size will be 2
-  bioloid.readPose();//find where the servos are currently
-  bioloid.setNextPose(PAN,pan);//prepare the PAN servo to the centered position, pan/2048
-  bioloid.setNextPose(TILT,tilt);//preprare the tilt servo to the centered position, tilt/2048
-  bioloid.interpolateSetup(2000);//setup for interpolation from the current position to the positions set in setNextPose, over 2000ms
-  while(bioloid.interpolating > 0)  //until we have reached the positions set in setNextPose, execute the instructions in this loop
+  
+  delay(1000);  //wait for the bioloid controller to intialize
+  
+  bioloid.poseSize = 2;            //2 servos, so the pose size will be 2
+  bioloid.readPose();              //find where the servos are currently
+  bioloid.setNextPose(PAN,pan);    //prepare the PAN servo to the default position
+  bioloid.setNextPose(TILT,tilt);  //preprare the tilt servo to the default position
+  bioloid.interpolateSetup(2000);  //setup for interpolation from the current position to the positions set in setNextPose, over 2000ms
+  while(bioloid.interpolating > 0) //until we have reached the positions set in setNextPose, execute the instructions in this loop
   {
     bioloid.interpolateStep();//move servos 1 'step
     delay(3);
@@ -101,8 +107,9 @@ void setup(){
 
 
  
-void loop(){
-   //read analog values from joysticks
+void loop()
+{
+   //read analog values from joysticks. Each variable will hold a value in between 0-1023 corresponding to the location of that axis of the joystick
    joyPanVal = analogRead(JOYPAN);
    joyTiltVal = analogRead(JOYTILT);
        
@@ -110,43 +117,32 @@ void loop(){
    //deadzone for pan jotystick - only change the pan value if the joystick value is outside the deadband
    if(joyPanVal > DEADBANDHIGH || joyPanVal < DEADBANDLOW)
    {
+     //joyPanVal will hold a value between 0 and 1023 that correspods to the location of the joystick. The map() function will convert this value
+     //into a value between speed and -speed. This value can then be added to the current panValue to incrementley move ths servo 
      joyPanMapped = map(joyPanVal, 0, 1023, -speed, speed);
-     pan += joyPanMapped;
+     pan = pan + joyPanMapped;
+
+     //enforce upper/lower limits for pan servo. This will ensure we do not move the servo to a position out of its physical bounds. 
+     pan = max(pan, PAN_MIN);  //use the max() function to make sure the value never falls below PAN_MIN 
+     pan = min(pan, PAN_MAX);  //use the min() function to make sute the value never goes above PAN_MAX 
+
+
    }
     
    //deadzone for tilt jotystick - only change the pan value if the joystick value is outside the deadband  
    if(joyTiltVal > DEADBANDHIGH || joyTiltVal < DEADBANDLOW)
    {
+     //joyTiltVal will hold a value between 0 and 1023 that correspods to the location of the joystick. The map() function will convert this value
+     //into a value between speed and -speed. This value can then be added to the current panValue to incrementley move ths servo 
      joyTiltMapped = map(joyTiltVal, 0, 1023, -speed, speed);
-     tilt += joyTiltMapped;
+     tilt = tilt + joyTiltMapped;
+          
+     //enforce upper/lower limits for pan servo. This will ensure we do not move the servo to a position out of its physical bounds. 
+     tilt = max(tilt, TILT_MIN);  //use the max() function to make sure the value never falls below TILT_MIN 
+     tilt = min(tilt, TILT_MAX);  //use the min() function to make sute the value never goes above TILT_MAX 
+     
    }
-      
-    //enforce upper/lower limits for tilt servo
-    if (tilt < TILT_LOWER_LIMIT)
-    {
-      tilt =TILT_LOWER_LIMIT;
-  
-    }  
-    else if (tilt > TILT_UPPER_LIMIT)
-    {
-      tilt =TILT_UPPER_LIMIT;
-  
-    }
-    
-    
-    //enforce upper/lower limits for pan servo
-    if (pan < PAN_LOWER_LIMIT)
-    {
-      pan =PAN_LOWER_LIMIT;
-  
-    }  
-  
-    else if (pan > PAN_UPPER_LIMIT)
-    {
-      pan =PAN_UPPER_LIMIT;
-  
-    }
-    
+          
     //send pan and tilt goal positions to the pan/tilt servos 
     SetPosition(PAN,pan);
     SetPosition(TILT,tilt);
